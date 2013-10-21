50268418a9d7f6fe09b8fd10e6180707f01d2630
JAB <jab@charger>
2013-10-20 23:41:26 -0400

    du_by_owner is what it says instead of overall count/size

--- du_by_owner.c
+++ du_by_owner.c
@@ -19,6 +19,7 @@ static char size_key = 2;
 
 static int map(const char *fpath, const struct stat *sb, int tflag, void *kv) {
 	off_t size;
+	uid_t uid;
 	
 	switch (tflag) {
 		case FTW_D:
@@ -33,10 +34,9 @@ static int map(const char *fpath, const struct stat *sb, int tflag, void *kv) {
 			//ignore symlinks
 			if (!S_ISLNK(sb->st_mode)) {
 				size = sb->st_size;
-				MR_kv_add(kv, &size_key, 1, (char*)&size, sizeof(off_t));
-				
-				//the value here in theory is always 1, so don't even bother, just use NULL
-				MR_kv_add(kv, &count_key, 1, NULL, 0); 
+				uid  = sb->st_uid;
+				//MR_kv_add(kv, &size_key, 1, (char*)&size, sizeof(off_t));
+				MR_kv_add(kv, (char*)&uid, sizeof(uid_t), (char*)&size, sizeof(off_t));
 			}
 			return 0;
 		}
@@ -44,19 +44,18 @@ static int map(const char *fpath, const struct stat *sb, int tflag, void *kv) {
 }
 
 static void reduce(char *key, int keybytes, char *multivalue, int nvalues, int *valuebytes) {
-	if (*key==count_key) {
-		printf("total path count: %d\n", nvalues);
-	} else if (*key==size_key) {
-		uint64_t totalsize = 0;
+	uid_t uid;
+	uint64_t totalsize = 0;
 
-		int i;
-		int offset = 0;
-		for (i=0; i<nvalues; i++) {
-			totalsize += *(uint64_t *)(multivalue+offset);
-			offset += valuebytes[i];
-		}
-		printf("total path size: %ld\n", totalsize);
+	uid = *(uid_t *)key;
+
+	int i;
+	int offset = 0;
+	for (i=0; i<nvalues; i++) {
+		totalsize += *(uint64_t *)(multivalue+offset);
+		offset += valuebytes[i];
 	}
+	printf("uid %d: %ld bytes\n", uid, totalsize);
 }
 
 
