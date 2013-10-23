7cd8e128eb7b7538ada27f9b1021d8692ef9c666
John Brunelle <john_brunelle@harvard.edu>
2013-10-22 23:40:45 -0400

    more comments

--- examples/du_by_owner.c
+++ examples/du_by_owner.c
@@ -13,24 +13,31 @@ All rights reserved.
 
 #include "fsmr.h"
 
+//the map function
+//see ftw(3) man page (dftw is basically identical)
 static int map(const char *fpath, const struct stat *sb, int tflag, void *kv) {
 	off_t size;
 	uid_t uid;
 
 	switch (tflag) {
 		case FTW_D:
+			//fpath is a directory
 			return 0;
 		case FTW_DNR:
+			//fpath is a directory which can't be read
 			fprintf(stderr, "unreadable directory: %s\n", fpath);
 			return 1;
 		case FTW_NS:
+			//the stat(2) call failed on fpath, which is not a symbolic link
 			fprintf(stderr, "unstatable file: %s\n", fpath);
 			return 0;
 		default: {
+			//(FTW_F)
 			//ignore symlinks
 			if (!S_ISLNK(sb->st_mode)) {
 				size = sb->st_size;
 				uid  = sb->st_uid;
+				//emit a {"uid":size} key/value
 				MR_kv_add(kv, (char*)&uid, sizeof(uid_t), (char*)&size, sizeof(off_t));
 			}
 			return 0;
@@ -38,12 +45,15 @@ static int map(const char *fpath, const struct stat *sb, int tflag, void *kv) {
 	}
 }
 
+//the reduce function
+//see http://mapreduce.sandia.gov/doc/reduce.html
 static void reduce(char *key, int keybytes, char *multivalue, int nvalues, int *valuebytes) {
 	uid_t uid;
 	uint64_t totalsize = 0;
 
 	uid = *(uid_t *)key;
 
+	//loop over and sum all values for this key
 	int i;
 	int offset = 0;
 	for (i=0; i<nvalues; i++) {
